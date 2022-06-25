a9149ce8605cb351376cbcf8b9322c38367764c0
Andrea <andreaverace98@hotmail.it>
2022-06-25 11:30:18 +0200

    lastgame playmanagement

--- src/main/java/it/prova/pokeronline/web/api/PlayManagementController.java
+++ src/main/java/it/prova/pokeronline/web/api/PlayManagementController.java
@@ -9,8 +9,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import it.prova.pokeronline.dto.TavoloDTO;
 import it.prova.pokeronline.dto.UtenteDTO;
+import it.prova.pokeronline.model.Tavolo;
 import it.prova.pokeronline.model.Utente;
+import it.prova.pokeronline.service.TavoloService;
 import it.prova.pokeronline.service.UtenteService;
 
 @RestController
@@ -19,6 +22,9 @@ public class PlayManagementController {
 
 	@Autowired
 	private UtenteService utenteService;
+	
+	@Autowired
+	private TavoloService tavoloService;
 	
 	@PostMapping("/compraCredito/{credito}")
 	public UtenteDTO compraCredito(@PathVariable(required = true) int credito) {
@@ -31,5 +37,20 @@ public class PlayManagementController {
 		return UtenteDTO.buildUtenteDTOFromModel(utenteLoggato);
 	}
 	
+	
+	@GetMapping("/lastGame")
+	public TavoloDTO restituisciLastGame() {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		Utente utenteLoggato =  utenteService.findByUsername(auth.getName());
+		
+		for(Tavolo tavoloItem : tavoloService.listAllTavoli()) {
+			if(tavoloItem.getUtentiTavolo().contains(utenteLoggato)) {
+				return TavoloDTO.buildTavoloDTOFromModel(tavoloItem);
+			} 
+		}
+		return null;
+		
+		
+	}
 	
 }

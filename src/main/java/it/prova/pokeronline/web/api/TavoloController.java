6f2010aadbba121fc9871d8d2ac0a7be7cb01679
Andrea <andreaverace98@hotmail.it>
2022-06-25 10:03:04 +0200

    update tavolo

--- src/main/java/it/prova/pokeronline/web/api/TavoloController.java
+++ src/main/java/it/prova/pokeronline/web/api/TavoloController.java
@@ -9,19 +9,24 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.prova.pokeronline.dto.TavoloDTO;
+import it.prova.pokeronline.dto.UtenteDTO;
 import it.prova.pokeronline.model.Ruolo;
 import it.prova.pokeronline.model.Tavolo;
 import it.prova.pokeronline.model.Utente;
 import it.prova.pokeronline.service.TavoloService;
 import it.prova.pokeronline.service.UtenteService;
 import it.prova.pokeronline.web.api.exception.IdNotNullForInsertException;
+import it.prova.pokeronline.web.api.exception.TavoloNotFoundException;
+import it.prova.pokeronline.web.api.exception.UtenteNotFoundException;
 
 @RestController
 @RequestMapping("/api/tavolo")
@@ -59,6 +64,16 @@ public class TavoloController {
 			return TavoloDTO.buildTavoloDTOFromModel(tavoloInserito);
 		}
 	
-	
+		@PutMapping("/{id}")
+		public TavoloDTO update(@Valid @RequestBody TavoloDTO tavoloInput, @PathVariable(required = true) Long id) {
+			Tavolo tavolo = tavoloService.caricaSingoloTavolo(id);
+
+			if (tavolo == null)
+				throw new TavoloNotFoundException("Tavolo not found con id: " + id);
+
+			tavoloInput.setId(id);
+			Tavolo tavoloAggiornato = tavoloService.aggiorna(tavoloInput.buildTavoloModel(true));
+			return TavoloDTO.buildTavoloDTOFromModel(tavoloAggiornato);
+		}
 	
 }
